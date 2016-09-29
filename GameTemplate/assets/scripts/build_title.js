bb2dee96afc671aca02bff64cbeabcf4ec495026
Brandon <berningbrandon9@gmail.com>
2016-09-28 23:01:40 -0600

    added basic functionality

--- GameTemplate/assets/scripts/build_title.js
+++ GameTemplate/assets/scripts/build_title.js
@@ -1,5 +1,48 @@
-function buildTItle() {
+function buildAll() {
+    buildTitle();
+    buildInstructions();
+    showTitle();
+}
+
+function buildTitle() {
     titleScreen.x = 0;
     titleScreen.y = 0;
-    appendChild();
+    stage.addChild(titleScreen);
+
+    IButton.x = 10;
+    IButton.y = 10;
+    IButton.on("click", (showInstruction))
+    stage.addChild(IButton);
+}
+
+function buildInstructions() {
+    instructionScreen.x = 0;
+    instructionScreen.y = 0;
+    titleScreen.visible = false;
+    stage.addChild(instructionScreen)
+
+    TButton.x = 10;
+    TButton.y = 10;
+    TButton.on("click", (showTitle))
+    stage.addChild(TButton);
+}
+
+
+function showInstruction() {
+    hideAll();
+    instructionScreen.visible = true;
+    TButton.visible = true;
+}
+
+function showTitle() {
+    hideAll();
+    titleScreen.visible = true;
+    IButton.visible = true;
+}
+
+function hideAll() {
+    titleScreen.visible = false;
+    instructionScreen.visible = false;
+    IButton.visible = false;
+    TButton.visible = false;
 }
--- GameTemplate/script.js
+++ GameTemplate/script.js
@@ -1,15 +1,21 @@
-var stage, preload, queue;
+var stage, preload, queue, loader, gameState;
 
 var FPS = 30;
 var CANVAS_WIDTH = 800;
 var CANVAS_HEIGHT = 600;
 
-manifest = [
-    {src:"asstes/images/title.png", id:"title"},
-    {src:"asstes/images/gameArea.png", id:"background"},
-    {src:"asstes/images/Instruction.png", id:"instructions"},
-    {src:"asstes/images/GameOver.png", id:"gameover"}  
-];
+var gameStates = {
+    CONSTRUCT: 1,
+    TITLE: 2,
+    INSTRUCTION: 3,
+    START_GAME: 4,
+    IN_GAME: 5,
+    GAME_OVER: 6,
+    HOLD: 7
+}
+
+var titleScreen, backgroundScreen, instructionScreen, gameoverScreen;
+var IButton, TButton;
 
 function setupCanvas() {
     var canvas = document.getElementById("game"); //get canvas with id='game'
@@ -17,53 +23,63 @@ function setupCanvas() {
     canvas.height = CANVAS_HEIGHT;
     stage = new createjs.Stage(canvas); //makes stage object from the canvas
 }
- 
+
 function loadFiles() {
-    queue = new createjs.LoadQueue(true, "assets/images");  //files are stored in 'images' directory
-    queue.on("complete", loadComplete, this);  //when loading is done run 'loadComplete()'
-    queue.loadManifest(manifest);  //load files listed in 'manifest'
+    queue = new createjs.LoadQueue(true, "assets/images"); //files are stored in 'images' directory
+    queue.on("complete", loadComplete, this); //when loading is done run 'loadComplete()'
+    queue.loadManifest(manifest); //load files listed in 'manifest'
 }
- 
+
 function loadComplete(evt) {
     //once the files are loaded, put them into usable objects
     titleScreen = new createjs.Bitmap(queue.getResult("title"));
     backgroundScreen = new createjs.Bitmap(queue.getResult("background"));
     instructionScreen = new createjs.Bitmap(queue.getResult("instructions"));
     gameoverScreen = new createjs.Bitmap(queue.getResult("gameover"));
+    IButton = new createjs.Bitmap(queue.getResult("IButton"));
+    TButton = new createjs.Bitmap(queue.getResult("TButton"));
+    startLoop();
 }
 
+function startLoop() {
+    var FPS = 30;
+    createjs.Ticker.addEventListener('tick', loop);
+    createjs.Ticker.setFPS(FPS);
+    buildAll();
+}
+
+
+
 function loop() {
-    runGameTImer();
-    inLoopMusicMuteCHeck();
-    switch (gamestate) {
-        case CONSTRUCT:
+    // runGameTimer();
+    // inLoopMusicMuteCHeck();
+    switch (gameState) {
+        case gameStates.CONSTRUCT:
+
             gameState = HOLD;
-        break;
-        case TITLE:
-        break;
-        case INSTRUCTION:
-        break;
-        case START_GAME:
+            break;
+        case gameStates.TITLE:
+            break;
+        case gameStates.INSTRUCTION:
+            break;
+        case gameStates.START_GAME:
             startGame();
             showGame();
             hideTitle();
             playMusic();
-            gameState = START_LEVEL;
-        break;
-        case START_LEVEL:
-            gameState = hold;
-        break;
-        case IN_GAME:
+            gameState = IN_GAME;
+            break;
+        case gameStates.IN_GAME:
             gameLoop();
-            if(gameTimer > 10) {
-                gameState = GAME_OVER;
+            if (gameTimer > 10) {
+                gameStates = GAME_OVER;
                 stopMusic();
             }
-        break;
-        case GAME_OVER:
+            break;
+        case gameStates.GAME_OVER:
             showGameOver();
             hideGame();
-        break;
+            break;
         default:
     }
     stage.update();
@@ -71,6 +87,19 @@ function loop() {
 
 
 (function main() {
+
+    gameState = gameStates.HOLD;
+
+    manifest = [
+        { src: "/assets/scripts/build_title.js" },
+        { src: "/assets/images/title.png", id: "title" },
+        { src: "/assets/images/gameArea.png", id: "background" },
+        { src: "/assets/images/Instruction.png", id: "instructions" },
+        { src: "/assets/images/GameOver.png", id: "gameover" },
+        { src: "/assets/images/buttonInstructions.png", id: "IButton" },
+        { src: "/assets/images/buttonTitle.png", id: "TButton" }
+    ];
+
     setupCanvas();
     loadFiles();
 })();
